620aa59dcd2213fed55963239b5d8aff0faf58bb
32avad <2337678164@qq.com>
2026-01-11 20:26:20 +0800

    11

--- data.js
+++ data.js
@@ -3,6 +3,6 @@ const products = [
         "title": "写真",
         "price": "112",
         "desc": "50",
-        "img": "https://github.com/32avad/website/blob/main/ScreenShot_2026-01-09_210040_510.png"
+        "img": "https://cdn.jsdelivr.net/gh/32avad/website@main/ScreenShot_2026-01-09_210040_510.png"
     }
 ];
