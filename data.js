b312ab6fca76dc22d1076c0220b205d2815b470b
32avad <2337678164@qq.com>
2026-01-11 20:09:07 +0800

    写真1

--- data.js
+++ data.js
@@ -1,1 +1,8 @@
-const products = [];
+const products = [
+    {
+        "title": "写真",
+        "price": "112",
+        "desc": "50",
+        "img": "https://github.com/32avad/website/blob/main/ScreenShot_2026-01-09_210040_510.png"
+    }
+];
